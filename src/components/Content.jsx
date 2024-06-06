877a2225503bbc51219d38cd813d4cc0fb5bd7ea
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-06-06 13:34:15 +0530

    add code for base content page

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,39 @@
 import { useEffect, useState } from "react";
+import { PostCard } from "./postCard";
 
 export const Content = () => {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
 
-  useEffect;
-  return <>Content Page</>;
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("backendapi", { mode: "cors" });
+        if (!response.ok) {
+          throw new Error(`Server error: Status ${response.status}`);
+        }
+        const newPosts = await response.json();
+        setPosts(newPosts);
+        setError(null);
+      } catch (err) {
+        setError(err);
+        setPosts([]);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      {error ? (
+        <p>A network error was encountered: {error.message}</p>
+      ) : (
+        <>
+          {posts.map((post) => (
+            <PostCard key={post._id}></PostCard>
+          ))}
+        </>
+      )}
+    </>
+  );
 };
