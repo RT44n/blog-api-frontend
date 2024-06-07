c705df3ffd309570bf93a0cfe825cf41fdbdc579
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-06-07 15:17:55 +0530

    create auth logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import "./App.css";
 import { Header } from "./components/Header";
 import { Content } from "./components/Content";
 import { Sidebar } from "./components/Sidebar";
+import AuthProvider from "./utilities/auth";
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container mx-auto px-4 lg:px-0 max-w-7xl">
-        <Content />
-        <Sidebar />
-      </div>
+      <AuthProvider>
+        <Header />
+        <div className="container mx-auto px-4 lg:px-0 max-w-7xl">
+          <Content />
+          <Sidebar />
+        </div>
+      </AuthProvider>
     </>
   );
 }
